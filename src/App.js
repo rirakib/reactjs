e2dc33a58e8b27f0ef6f60477f631ad5daddada1
rirakib <rirrakib10@gmail.com>
2023-09-14 08:42:20 -0700

    react router dom practice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Navbar from './Navbar'
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello react js</h1>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </>
   )
 }
 
